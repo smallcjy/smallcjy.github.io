547d3b7b5477c9276bbe908eb079e889aa70e95e
smallcjy <2628035541@qq.com>
2024-10-08 21:31:13 +0800

    Site updated: 2024-10-08 21:31:12

--- js/boot.js
+++
@@ -1,3 +0,0 @@
-// build time:Wed Oct 02 2024 17:39:14 GMT+0800 (中国标准时间)
-Fluid.boot={};Fluid.boot.registerEvents=function(){Fluid.events.billboard();Fluid.events.registerNavbarEvent();Fluid.events.registerParallaxEvent();Fluid.events.registerScrollDownArrowEvent();Fluid.events.registerScrollTopArrowEvent();Fluid.events.registerImageLoadedEvent()};Fluid.boot.refresh=function(){Fluid.plugins.fancyBox();Fluid.plugins.codeWidget();Fluid.events.refresh()};document.addEventListener("DOMContentLoaded",function(){Fluid.boot.registerEvents()});
-//rebuild by hrmmi 
