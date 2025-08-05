95023677f1253200a34b79e9f4d958b58e89268b
smallcjy <2628035541@qq.com>
2025-08-05 21:07:03 +0800

    0805

--- test.cpp
+++ test.cpp
@@ -1,24 +1,81 @@
-// 动态规划：dp[i]: i个节点组成的二叉树共有dp[i]种情况
-// 思路：因为所有节点的孩子数为0或2，好二叉树的左右子树一定也是好二叉树，所以根据左右子树的好二叉树情况可以组合推断出该节点数的情况。
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-const int MOD = 1e9+7;
 
-int main()
-{
-    int n;
-    cin>>n;
-    vector<int> dp(n+1, 0); // 偶数节点数的树组不成好二叉树
-    dp[1] = 1; dp[3] = 1; // 初始化
+const int dirs[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
-    for(int i = 5; i <= n; i += 2) {
-        for(int j = 1; j < i - 1; j += 2) {
-            dp[i] += dp[j] * dp[i - j - 1];
-            dp[i] %= MOD;
+struct State {
+    int x, y;
+    int time;
+    // 用于 priority_queue 的比较，时间小的优先
+    bool operator<(const State& other) const {
+        return time > other.time;  // 最小堆
+    }
+};
+
+int main() {
+    int m, n;
+    cin >> m >> n;
+    vector<vector<int>> grid(m, vector<int>(n));
+    int j, k;
+    cin >> j >> k;
+
+    // 输入网格
+    for (int i = 0; i < m; i++) {
+        for (int l = 0; l < n; l++) {
+            cin >> grid[i][l];
         }
     }
 
-    cout<<dp[n];
+    // Dijkstra 初始化
+    vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
+    priority_queue<State> pq;
+    dist[j][k] = 0;
+    pq.push({j, k, 0});
+
+    while (!pq.empty()) {
+        State cur = pq.top();
+        pq.pop();
+
+        // 如果当前时间已经大于记录的最短时间，跳过
+        if (cur.time > dist[cur.x][cur.y]) {
+            continue;
+        }
+
+        // 遍历四个方向
+        for (auto& dir : dirs) {
+            int nx = cur.x + dir[0];
+            int ny = cur.y + dir[1];
+
+            // 检查边界和是否为空地
+            if (nx < 0 || nx >= m || ny < 0 || ny >= n || grid[nx][ny] == 0) {
+                continue;
+            }
+
+            // 计算新的传播时间
+            int new_time = cur.time + grid[cur.x][cur.y];
+
+            // 如果更优，更新并加入堆
+            if (new_time < dist[nx][ny]) {
+                dist[nx][ny] = new_time;
+                pq.push({nx, ny, new_time});
+            }
+        }
+    }
+
+    // 检查所有信号塔是否可达
+    int max_time = 0;
+    for (int i = 0; i < m; i++) {
+        for (int l = 0; l < n; l++) {
+            if (grid[i][l] != 0) {  // 是信号塔
+                if (dist[i][l] == INT_MAX) {
+                    cout << -1 << endl;
+                    return 0;
+                }
+                max_time = max(max_time, dist[i][l]);
+            }
+        }
+    }
+
+    cout << max_time << endl;
     return 0;
 }
