9900b6a3aa7a93aec772b815601f364384fd4bc0
smallcjy <2628035541@qq.com>
2025-08-11 21:11:36 +0800

    0810

--- test.cpp
+++ test.cpp
@@ -1,65 +1,35 @@
 #include<bits/stdc++.h>
-#include<limits.h>
 using namespace std;
 
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-};
+int main()
+{
+    int n, m;
+    cin>>n>>m;
+    string str;
+    cin>>str;
 
-TreeNode* buildTreeFromLevelOrder(const vector<string>& nodes) {
-    if (nodes.empty() || nodes[0] == "null") return nullptr;
-    
-    TreeNode* root = new TreeNode(stoi(nodes[0]));
-    queue<TreeNode*> q;
-    q.push(root);
-    
-    int i = 1;
-    while (!q.empty() && i < nodes.size()) {
-        TreeNode* curr = q.front();
-        q.pop();
-        
-        if (i < nodes.size() && nodes[i] != "null") {
-            curr->left = new TreeNode(stoi(nodes[i]));
-            q.push(curr->left);
+    for(int i = 0; i < str.size(); i++) {
+        if (str[i] == '?') {
+            bool flag = false;
+            for(char j = 'a'; j < 'a' + m; j++) {
+                if ((i==0 || str[i-1] != j) && (i==str.size()-1 || str[i+1] != j)) {
+                    flag = true;
+                    str[i] = j;
+                }
+
+                if (!flag) {
+                    str[i] = str[i-1];
+                }
+            }
         }
-        i++;
-        
-        if (i < nodes.size() && nodes[i] != "null") {
-            curr->right = new TreeNode(stoi(nodes[i]));
-            q.push(curr->right);
-        }
-        i++;
     }
-    
-    return root;
-}
 
-int res = 0;
+    int ans = 0;
+    cout<<str<<endl;
+    for(int i = 0; i < str.size() - 1; i++) {
+        ans += str[i] != str[i+1] ? 1 : 0;
+    }
 
-int dfs(TreeNode* root) {
-    if (!root) return 0;
-    if (!root->left && !root->right) return 1;
-
-    int l = dfs(root->left);
-    int r = dfs(root->right);
-
-    int lv = (root->left && root->val == root->left->val) ? l : 0;
-    int rv = (root->right && root->val == root->right->val) ? r : 0;
-
-    res = max(res, 1 + lv + rv);
-    return 1 + max(lv, rv);
-}
-
-int main() {
-    int n;
-    cin >> n;
-    vector<string> strs(n);
-    for (int i = 0; i < n; i++) cin >> strs[i];
-    TreeNode* root = buildTreeFromLevelOrder(strs);
-    dfs(root);
-    cout << res; // 输出 res 而非 dfs(root) 的返回值
+    cout<<ans;
     return 0;
 }
