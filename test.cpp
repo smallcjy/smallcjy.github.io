5a8afd20bb83753d1373d45701ed390f2d0f1a35
smallcjy <2628035541@qq.com>
2025-08-16 15:03:41 +0800

    0816

--- test.cpp
+++ test.cpp
@@ -1,70 +1,45 @@
-/**
- * Definition for a point.
- * struct Point {
- *     int x;
- *     int y;
- *     Point() : x(0), y(0) {}
- *     Point(int a, int b) : x(a), y(b) {}
- * };
- */
-
-struct Point {
-    int x;
-    int y;
-};
-
+#include<limits.h>
 #include<bits/stdc++.h>
 using namespace std;
 
-const int dirs_x[8] = {1, 1, 2, 2, -1, -1, -2, -2};
-const int dirs_y[8] = {2, -2, 1, -1, 2, -2, 1, -1};
 
 class Solution {
 public:
     /**
-     * @param grid: a chessboard included 0 (false) and 1 (true)
-     * @param source: a point
-     * @param destination: a point
-     * @return: the shortest path 
+     * @param maze: the maze
+     * @param start: the start
+     * @param destination: the destination
+     * @return: the shortest distance for the ball to stop at the destination
      */
-    int shortestPath(vector<vector<bool>> &grid, Point &source, Point &destination) {
-        int n = grid.size(),m = grid[0].size();
-        if (n == 0 || m == 0) {
-            return -1;
+    const int dirs[5] = {1, 0, -1, 0, 1};
+
+    struct Point {
+        int x, y;
+    };
+
+    int ans = INT_MAX;
+
+    vector<string> wordSort(vector<char> &alphabet, vector<string> &words) {
+        // Write your code here
+        char into[500], outto[500];
+        for(int i = 0; i < alphabet.size(); i++) {
+            into[alphabet[i]] = 'a' + i;
+            outto['a' + i] = alphabet[i];
         }
         
-        int dx[] = {1, 1, 2, 2, -1, -1, -2, -2};
-        int dy[] = {2, -2, 1, -1, 2, -2, 1, -1};
-        queue<Point> q;
-        q.push(source);
-        grid[source.x][source.y] = true;
-        int ans = 0;
-        while (!q.empty()) {
-            int len_q = q.size();
-            for (int k = 0; k < len_q; k++) {
-                Point cur = q.front();
-                q.pop();
-                //到达终点，返回距离
-                if (cur.x == destination.x && cur.y == destination.y) {
-                    return ans;
-                }
-                for (int i = 0; i < 8; i++) {
-                    Point next;
-                    next.x = cur.x + dx[i];
-                    next.y = cur.y + dy[i];
-                    //判断下一跳可否到达
-                    if (is_in_bound(next,n,m) && grid[next.x][next.y] == false) {
-                        q.push(next);
-                        grid[next.x][next.y] = true;
-                    }
-                }
+        for(auto& word : words) {
+            for(int i = 0; i < word.size(); i++) {
+                word[i] = into[word[i]];
             }
-            ans++;
         }
-        return -1;
-    }
-    //判断下一跳是否越界
-    bool is_in_bound(Point next,int n,int m) {
-        return 0 <= next.x && next.x < n && 0 <= next.y && next.y < m;
+
+        sort(words.begin(), words.end());
+        for(auto& word : words) {
+            for(int i = 0; i < word.size(); i++) {
+                word[i] = outto[word[i]];
+            }
+        }
+
+        return words;
     }
 };
