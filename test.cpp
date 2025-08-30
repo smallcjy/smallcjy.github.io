13667b4acae3f912763417a8fdc3ff147c06912d
smallcjy <2628035541@qq.com>
2025-08-30 17:54:02 +0800

    0830

--- test.cpp
+++ test.cpp
@@ -1,28 +1,9 @@
 #include <bits/stdc++.h>
-using Seed = std::mt19937;
+using namespace std;
 
-// Returns a random unsigned integer in the range [min, max]
-size_t NextRandUInt(size_t seed, size_t min, size_t max) {
-    Seed rng(seed);
-    std::uniform_int_distribution<size_t> dist(min, max);
-    return dist(rng);
-}
-
-// Returns a random double in the range [min, max)
-double NextRandDouble(size_t seed, double min, double max) {
-    Seed rng(seed);
-    std::uniform_real_distribution<double> dist(min, max);
-    return dist(rng);
-}
-
-int main() {
-    size_t timestamp = std::chrono::system_clock::now().time_since_epoch().count();
-
-    std::cout<<NextRandUInt(timestamp, 0, 100)<<std::endl;
-    std::cout<<NextRandUInt(timestamp, 0, 100)<<std::endl;
-
-    std::cout<<NextRandUInt(timestamp, 1000, 10000)<<std::endl;
-    std::cout<<NextRandUInt(timestamp, 1000, 10000)<<std::endl;
-
+int main()
+{
+    list<int> dq = {1, 2, 3, 4, 5};
+    cout << *dq.begin() + 2;
     return 0;
 }
