4ab9612ac5b8ccb276c8c0e66be5be5070e66997
smallcjy <2628035541@qq.com>
2025-09-23 16:16:42 +0800

    0923

--- test.cpp
+++ test.cpp
@@ -1,9 +1,78 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int main()
-{
-    list<int> dq = {1, 2, 3, 4, 5};
-    cout << *dq.begin() + 2;
+struct Node {
+    int val; Node* next;
+    Node(int val_) : val(val_), next(nullptr) {}
+};
+
+Node* build(vector<int>& nums) {
+	Node* dummy = new Node(0);
+   	Node* cur = dummy;
+    for(int n : nums) {
+        cur->next = new Node(n);
+        cur = cur->next;
+    }
+    
+    return dummy->next;
+}
+
+Node* merge(Node* a, Node* b) {
+    Node* dummy = new Node(0);
+    Node* cur = dummy;
+    
+    while(a && b) {
+        if (a->val <= b->val) {
+            cur->next = a;
+            a = a->next;
+        } else {
+            cur->next = b;
+            b = b->next;
+        }
+       	cur = cur->next;
+    }
+    
+    if (a) cur->next =a;
+    else if (b) cur->next = b;
+    return dummy->next;
+}
+
+Node* sort(Node* head) {
+    if (!head || !head->next) return head;
+    Node* dummy = new Node(0); dummy->next = head;
+    
+    Node* fast = head;
+    Node* slow = dummy;
+    
+    while(fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    
+    Node* right = slow->next;
+    slow->next = nullptr;
+    Node* left = head;
+    
+    Node* sortl = sort(left);
+    Node* sortr = sort(right);
+    
+    return merge(sortl, sortr);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for(int i = 0; i < n; i++) cin>>nums[i];
+    Node* head = build(nums);
+    Node* ans = sort(head);
+    
+    while(ans) {
+        cout << ans->val << ' ';
+        ans = ans->next;
+    }
+    
     return 0;
 }
+// 64 位输出请用 printf("%lld")
